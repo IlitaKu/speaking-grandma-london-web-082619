11aaf41d565f1a6cf7407ae509e729f0fcf1ad6f
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-20 13:50:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase ==phrase.uppercase
   return "NO, NOT SINCE 1939!"
-elsif phrase === "I LOVE YOU GRANDMA!"
+else phrase === "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PIMPKIN!"
   end
 end
